affb35ccf892831aa83a14be627ab9e19385fbf5
Maarten van Meersbergen <m.vanmeersbergen@esciencecenter.nl>
2013-03-29 17:31:25 +0100

    Moved eSight library jar to lib.

--- src/nl/esciencecenter/asterisk/AsteriskGLEventListener.java
+++ src/nl/esciencecenter/asterisk/AsteriskGLEventListener.java
@@ -27,6 +27,7 @@ import nl.esciencecenter.esight.exceptions.InverseNotAvailableException;
 import nl.esciencecenter.esight.exceptions.UninitializedException;
 import nl.esciencecenter.esight.input.InputHandler;
 import nl.esciencecenter.esight.math.Color4;
+import nl.esciencecenter.esight.math.MatF3;
 import nl.esciencecenter.esight.math.MatF4;
 import nl.esciencecenter.esight.math.MatrixFMath;
 import nl.esciencecenter.esight.math.Point4;
@@ -45,6 +46,25 @@ import nl.esciencecenter.esight.text.jogampExperimental.FontFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/* Copyright [2013] [Netherlands eScience Center]
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Maarten van Meersbergen <m.van.meersbergen@esciencecenter.nl>
+ * 
+ */
 public class AsteriskGLEventListener implements GLEventListener {
     private final static Logger logger = LoggerFactory
             .getLogger(AsteriskGLEventListener.class);
@@ -412,8 +432,8 @@ public class AsteriskGLEventListener implements GLEventListener {
         starHaloShader
                 .setUniformMatrix("SMatrix", MatrixFMath.scale((settings
                         .getParticleSizeMultiplier() * .2f)));
-        starHaloShader.setUniformMatrix("NormalMatrix",
-                MatrixFMath.getNormalMatrix(mv));
+
+        starHaloShader.setUniformMatrix("NormalMatrix", new MatF3());
 
         VecF3 cameraPos;
         try {
