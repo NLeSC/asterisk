bafe3e16e3372056795d6a01dae8ca8db888c406
Maarten van Meersbergen <m.vanmeersbergen@esciencecenter.nl>
2014-01-16 11:49:03 +0100

    Added an additional preset to the settings for point clouds..

--- src/nl/esciencecenter/asterisk/AsteriskInterfaceWindow.java
+++ src/nl/esciencecenter/asterisk/AsteriskInterfaceWindow.java
@@ -263,7 +263,22 @@ public class AsteriskInterfaceWindow extends JPanel {
                 createPointCloudPanel(pointCloudConfig);
             }
         };
-        ActionListener prettyPresetListener = new ActionListener() {
+        ActionListener embellishedPresetListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                settings.setPointGasPointSizeDependantOnCameraDistance(false);
+                settings.setPointGasPointSizeSetting(1);
+                settings.setPointGasBlurPassSetting(3);
+                settings.setPointGasBlurSizeSetting(3);
+                settings.setPointGasBlurTypeSetting(6);
+                settings.setPostprocessingPointGasBrightness(25);
+
+                pointCloudConfig.removeAll();
+                createPointCloudPanel(pointCloudConfig);
+
+            }
+        };
+        ActionListener gaseousPresetListener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 settings.setPointGasPointSizeDependantOnCameraDistance(false);
@@ -280,7 +295,8 @@ public class AsteriskInterfaceWindow extends JPanel {
         };
 
         targetPanel.add(GoggleSwing.buttonBox("Presets", new GoggleSwing.ButtonBoxItem("Scientific",
-                scientificPresetListener), new GoggleSwing.ButtonBoxItem("Pretty", prettyPresetListener)));
+                scientificPresetListener), new GoggleSwing.ButtonBoxItem("Embellished", embellishedPresetListener),
+                new GoggleSwing.ButtonBoxItem("Gaseous", gaseousPresetListener)));
 
         targetPanel.add(GoggleSwing.checkboxBox("", new GoggleSwing.CheckBoxItem("Size Dependant on camera Distance",
                 settings.isPointgasSizeDependantOnCameraDistance(), pointGasDistanceDependantSizeListener)));
